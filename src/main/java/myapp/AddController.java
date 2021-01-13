976d544b8ff1a95b3b0e1527e65421dfcace5eab
Pramod Acharya <pramoddotacharya@gmail.com>
2021-01-12 20:45:53 -0600

    Ititial commit

---
+++ src/main/java/myapp/AddController.java
@@ -0,0 +1,22 @@
+package myapp;
+
+	import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+	@RestController
+	public class AddController {
+
+		@GetMapping( "/addByTen/{num}")
+		public ResponseEntity<Long>  addByTen(@PathVariable Long num) {
+			return  new ResponseEntity<>(num+10, HttpStatus.CREATED);
+		}
+		
+		@GetMapping( "/add/{num1}/{num2}")
+		public ResponseEntity<Long>  add(@PathVariable Long num1, @PathVariable Long num2) {
+			return  new ResponseEntity<>(num1+num2, HttpStatus.CREATED);
+		}
+
+	}
